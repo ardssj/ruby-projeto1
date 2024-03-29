60f214d8536bccb81d69301947a574761f585b2e
André Sousa <109698651+ardssj@users.noreply.github.com>
2024-03-28 21:18:38 -0300

    Add files via upload

---
+++ aula4.rb
@@ -0,0 +1,13 @@
+puts "Digite o seu nome: "
+
+nome = gets.chomp
+
+puts "Digite o seu nome: "
+
+sobrenome = gets.chomp
+
+puts "Digite a sua idade: "
+
+idade = gets.chomp.to_i
+
+puts "Olá, #{nome} #{sobrenome}! Você tem #{idade} anos."
